ec0a2a8b17cc797e2358678d66b2df574ead25dc
Evan La Fontaine <navelafontaine@gmail.com>
2026-06-17 11:13:56 +1000

    Write driver for IMu

--- imu.cpp
+++ imu.cpp
@@ -1,17 +1,59 @@
 #include "imu.hpp"
 
-Imu::Imu() {
-
+Imu::Imu(TwoWire& w) : w{&w} {
+  write_reg(kPwrMgmt1Reg, 0x01);
+  write_reg(kSmplrtDivReg, 0x00);
+  write_reg(kConfigReg, 0x00);
+  // Second lowest range for both
+  write_reg(kGyroConfigReg, 0x08);
+  write_reg(kAccelConfigReg, 0x08);
 }
 
-float Imu::ax() {
-  return 0.0f;
+float to_float(uint16_t x) {
+  return static_cast<float>(static_cast<int16_t>(
+    ((x & 0x00FF) << 8) +
+    ((x & 0xFF00) >> 8)
+  ));
 }
 
-float Imu::ay() {
-  return 0.0f;
+void Imu::update() {
+  w->beginTransmission(kAddr);
+  w->write(kAccelOutReg);
+  w->endTransmission(false);
+  w->requestFrom(kAddr, static_cast<uint8_t>(14));
+
+  uint16_t raw_data[7]; // [ax,ay,az,temp,gx,gy,gz]
+  w->readBytes(reinterpret_cast<char*>(raw_data), 14);
+
+  m_ax = static_cast<float>(to_float(raw_data[0])) * kAccFactor;
+  m_ay = static_cast<float>(to_float(raw_data[1])) * kAccFactor;
+  m_gz = static_cast<float>(to_float(raw_data[6])) * kGyroFactor;
 }
 
-float Imu::gz() {
-  return 0.0f;
+float Imu::ax() const {
+  return m_ax;
+}
+
+float Imu::ay() const {
+  return m_ay;
+}
+
+float Imu::gz() const {
+  return m_gz;
+}
+
+void Imu::write_reg(uint8_t reg, uint8_t data) {
+  w->beginTransmission(kAddr);
+  w->write(reg);
+  w->write(data);
+  w->endTransmission();
+}
+
+uint8_t Imu::read_reg(uint8_t reg) {
+  w->beginTransmission(kAddr);
+  w->write(reg);
+  w->endTransmission(true);
+
+  w->requestFrom(kAddr, static_cast<uint8_t>(1));
+  return w->read();
 }
--- imu.hpp
+++ imu.hpp
@@ -1,22 +1,44 @@
 #pragma once
 
+#include <Arduino.h>
+#include <Wire.h>
+
 class Imu {
   public:
-    // TODO: add required args
-    Imu();
+    Imu() = default;
+    Imu(TwoWire& w);
+
+    void update();
 
     // Linear acceleration in the x direction in m/s^2
     // fwd = +ve, rev = -ve
-    float ax();
+    float ax() const;
 
     // Linear acceleration in the y direction in m/s^2
     // left = +ve, right = -ve
-    float ay();
+    float ay() const;
 
     // Angular velocity about the z axis in rad/s
     // ccw = +ve, cw = -ve
-    float gz();
+    float gz() const;
 
   private:
-    // TODO: add member variables
+    void write_reg(uint8_t addr, uint8_t data);
+    uint8_t read_reg(uint8_t addr);
+
+    TwoWire* w;
+    float m_ax{0.0};
+    float m_ay{0.0};
+    float m_gz{0.0};
+
+    constexpr static uint8_t kAddr{0x68};
+    constexpr static uint8_t kPwrMgmt1Reg{0x6b};
+    constexpr static uint8_t kSmplrtDivReg{0x19};
+    constexpr static uint8_t kConfigReg{0x1a};
+    constexpr static uint8_t kGyroConfigReg{0x1b};
+    constexpr static uint8_t kAccelConfigReg{0x1c};
+    constexpr static uint8_t kAccelOutReg{0x3B};
+
+    constexpr static float kAccFactor{9.81f/8192.0f};
+    constexpr static float kGyroFactor{1.0f/1.143191f};
 };
