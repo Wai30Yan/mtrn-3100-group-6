1ae33977ace858c78a921e1fa94c295277800a97
Evan La Fontaine <navelafontaine@gmail.com>
2026-06-17 11:40:50 +1000

    Add skeleton for encoder

---
+++ encoder.cpp
@@ -0,0 +1,9 @@
+#include "encoder.hpp"
+
+Encoder::Encoder() {
+
+}
+
+float Encoder::position() const {
+  return 0.0f;
+}
---
+++ encoder.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+class Encoder {
+  public:
+    // TODO: add required args
+    Encoder();
+
+    // Get the current position of the encoder in radians
+    float position() const;
+
+  private:
+    // TODO: add member variables
+}
