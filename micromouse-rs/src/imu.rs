365db9c3b8a2398d3ea9d5c6903ba6f1bf6e7b4a
Evan La Fontaine <navelafontaine@gmail.com>
2026-07-06 18:29:56 +1000

    Port motors

--- micromouse-rs/src/imu.rs
+++ micromouse-rs/src/imu.rs
@@ -1,3 +1,5 @@
+use core::f32;
+
 use crate::I2cBus;
 
 pub struct Imu<'a> {
@@ -10,13 +12,13 @@ pub struct Imu<'a> {
 
 const ADDRESS: u8 = 0x68;
 const ACCEL_FACTOR: f32 = 9.81 / 8192.0;
-const GYRO_FACTOR: f32 = 1.0 / 1.143191;
+const GYRO_FACTOR: f32 = f32::consts::PI / (32.75 * 180.0);
 
 const PWR_MGMT_1_REG: u8 = 0x6b;
 const SMPLRT_DIV_REG: u8 = 0x19;
 const CONFIG_REG: u8 = 0x26;
-const GYRO_CONFIG_REG: u8 = 0x27;
-const ACCEL_CONFIG_REG: u8 = 0x28;
+const GYRO_CONFIG_REG: u8 = 0x1B;
+const ACCEL_CONFIG_REG: u8 = 0x1C;
 
 impl<'a> Imu<'a> {
     pub fn new(i2c_bus: &'a I2cBus<'a>) -> Self {
@@ -57,9 +59,9 @@ impl<'a> Imu<'a> {
             .write_read(ADDRESS, &[0x3b], &mut buf)
             .unwrap();
 
-        self.ax = i16::from_le_bytes(*buf[0..1].as_array().unwrap()) as f32 * ACCEL_FACTOR;
-        self.ay = i16::from_le_bytes(*buf[2..3].as_array().unwrap()) as f32 * ACCEL_FACTOR;
-        self.gz = i16::from_le_bytes(*buf[6..7].as_array().unwrap()) as f32 * GYRO_FACTOR;
+        self.ax = i16::from_be_bytes(buf[0..2].try_into().unwrap()) as f32 * ACCEL_FACTOR;
+        self.ay = i16::from_be_bytes(buf[2..4].try_into().unwrap()) as f32 * ACCEL_FACTOR;
+        self.gz = i16::from_be_bytes(buf[12..14].try_into().unwrap()) as f32 * GYRO_FACTOR;
     }
 
     /// Linear acceleration in the x direction in m/s^2
