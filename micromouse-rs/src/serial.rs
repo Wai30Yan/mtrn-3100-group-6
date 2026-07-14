b6239e0d3a4cb9da77553bc3352a173573457ea8
Evan La Fontaine <navelafontaine@gmail.com>
2026-07-14 17:12:35 +1000

    Fix timing issues and run formatter

--- micromouse-rs/src/serial.rs
+++ micromouse-rs/src/serial.rs
@@ -12,6 +12,10 @@
 
 use core::{
     cell::UnsafeCell,
+<<<<<<< HEAD
+=======
+    intrinsics::breakpoint,
+>>>>>>> b4a1a27 (Fix timing issues and run formatter)
     mem::{MaybeUninit, swap},
 };
 
